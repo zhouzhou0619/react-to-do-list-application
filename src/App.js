bccfd6ba2ad6e1cb73c3e1b82b42999f30c5954f
zhouzhou <zhouzzhou0619@gmail.com>
2023-07-28 14:11:22 +1000

    Add todo function, use console log to check

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import TodoList from './components/TodoList';
 
 function App() {
     return (
         <div className="todo-app">
-            <h1>Todo app</h1>
+            <TodoList />
         </div>
     );
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
+
+function TodoList() {
+    const [todos, setTodos] = useState([]);
+
+    const addTodo = todo => {
+        if (!todo.text || /^\s*$/.test(todo.text)) {
+            return;
+        }
+
+        const newTodos = [todo, ...todos];
+        setTodos(newTodos);
+        console.log(todo, ...todos);
+    };
+    return (
+        <div>
+            <h1>What's the Plan for Today?</h1>
+            <TodoForm onSubmit={addTodo} />
+        </div>
+    );
+}
+
+export default TodoList;
