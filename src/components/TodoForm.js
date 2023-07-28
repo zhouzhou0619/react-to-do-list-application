4804141aed4c8775987b362a19a6ea000b5f54b7
zhouzhou <zhouzzhou0619@gmail.com>
2023-07-28 15:41:24 +1000

    Add css

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 function TodoForm(props) {
     const [input, setInput] = useState('');
+
+    const inputRef = useRef(null);
+    useEffect(() => {
+        inputRef.current.focus();
+    });
 
     const handleChange = e => {
         setInput(e.target.value);
@@ -20,7 +25,7 @@ function TodoForm(props) {
 
     return (
         <form className="todo-form" onSubmit={handleSubmit}>
-            <input type="text" placeholder="Add a todo" value={input} name="text" className="todo-input" onChange={handleChange} />
+            <input type="text" placeholder="Add a todo" value={input} name="text" className="todo-input" onChange={handleChange} ref={inputRef} />
             <button className="todo-button">Add todo</button>
         </form>
     );
