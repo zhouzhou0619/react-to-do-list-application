c34cd99654f96a7883fbd9ef487f9f4640b2329f
zhouzhou <zhouzzhou0619@gmail.com>
2023-07-28 14:55:37 +1000

    make ""delete-icon" works

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import TodoForm from './TodoForm';
+import Todo from './Todo';
 
 function TodoList() {
     const [todos, setTodos] = useState([]);
@@ -12,10 +13,25 @@ function TodoList() {
         const newTodos = [todo, ...todos];
         setTodos(newTodos);
     };
+
+    const removeTodo = id => {
+        const removeArr = [...todos].filter(todo => todo.id !== id);
+        setTodos(removeArr);
+    };
+    const completeTodo = id => {
+        let updatedTodos = todos.map(todo => {
+            if (todo.id === id) {
+                todo.isComplete = !todo.isComplete;
+            }
+            return todo;
+        });
+        setTodos(updatedTodos);
+    };
     return (
         <div>
             <h1>What's the Plan for Today?</h1>
             <TodoForm onSubmit={addTodo} />
+            <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} />
         </div>
     );
 }
